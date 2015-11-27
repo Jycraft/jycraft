d248b7ac4123357af42fd707e6b54b7345eaddd5
Angel Daniel Munoz Gonzalez <scythes.lol@gmail.com>
2015-11-27 05:38:09 -0700

    changed tabs to spaces

--- src-common/jycraft/plugin/ConsolePlugin.java
+++ src-common/jycraft/plugin/ConsolePlugin.java
@@ -7,55 +7,55 @@ import jycraft.plugin.servers.SocketServer;
 import jycraft.plugin.servers.StaticFilesSever;
 
 public class ConsolePlugin {
-	private ConsolePlugin() {}
-		
-	public static void start(JyCraftPlugin mainPlugin, int telnetport, int websocketport, int staticserveport, String serverpass, String staticserverootdir, String staticservedir) {
-		if (telnetport > -1) {
-			SocketServer server = new SocketServer(mainPlugin, telnetport, serverpass);
-			Thread t = new Thread(server);
-			t.start();
-		}
-		if (websocketport > -1) {
-			PyWebSocketServer webserver = new PyWebSocketServer(mainPlugin, websocketport, serverpass);
-			webserver.start();
-		}
-		loadPythonPlugins("./python-plugins");
+    private ConsolePlugin() {}
+
+    public static void start(JyCraftPlugin mainPlugin, int telnetport, int websocketport, int staticserveport, String serverpass, String staticserverootdir, String staticservedir) {
+        if (telnetport > -1) {
+            SocketServer server = new SocketServer(mainPlugin, telnetport, serverpass);
+            Thread t = new Thread(server);
+            t.start();
+        }
+        if (websocketport > -1) {
+            PyWebSocketServer webserver = new PyWebSocketServer(mainPlugin, websocketport, serverpass);
+            webserver.start();
+        }
+        loadPythonPlugins("./python-plugins");
         // if a port has been specified, it has to be greater than 0
-		if (staticserveport > -1){
+        if (staticserveport > -1){
             // new file server instance
-			StaticFilesSever filesServer = new StaticFilesSever(staticserveport,staticserverootdir, staticservedir);
-			// call HttpServer.setup() in a wrapper method
+            StaticFilesSever filesServer = new StaticFilesSever(staticserveport,staticserverootdir, staticservedir);
+            // call HttpServer.setup() in a wrapper method
             filesServer.setupServer();
 
             // loop to serve the files
-			try {
-				filesServer.loopServer();
-			}
-			catch (InterruptedException e) {
-				System.out.printf("Static files server thread interrupted %s.\n", e.getMessage());
-			}
-		}
+            try {
+                filesServer.loopServer();
+            }
+            catch (InterruptedException e) {
+                System.out.printf("Static files server thread interrupted %s.\n", e.getMessage());
+            }
+        }
 
 
-	}
+    }
 
-	private static void loadPythonPlugins(String path) {
-		File pluginsDir = new File(path);
-		if (!pluginsDir.exists() || !pluginsDir.isDirectory())
-			return;
-		final File[] files = pluginsDir.listFiles();
-		for (final File file : files) {
-			if (!file.getName().endsWith(".py")) continue;
-			Thread pyPlugin = new Thread() {
-				@Override
-				public void run() {
-					PyInterpreter interpreter = new PyInterpreter();
-					interpreter.execfile(file.getAbsolutePath());
-					interpreter.close();
-				}
-			};
-			pyPlugin.start();
-		} 
-	}
-	
+    private static void loadPythonPlugins(String path) {
+        File pluginsDir = new File(path);
+        if (!pluginsDir.exists() || !pluginsDir.isDirectory())
+            return;
+        final File[] files = pluginsDir.listFiles();
+        for (final File file : files) {
+            if (!file.getName().endsWith(".py")) continue;
+            Thread pyPlugin = new Thread() {
+                @Override
+                public void run() {
+                    PyInterpreter interpreter = new PyInterpreter();
+                    interpreter.execfile(file.getAbsolutePath());
+                    interpreter.close();
+                }
+            };
+            pyPlugin.start();
+        }
+    }
+
 }
